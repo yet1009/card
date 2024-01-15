8755b4e3a3f4d2e6f168ed99ded9f2b40cfb6116
yet1009 <yeontae.y@ingstory.kr>
2024-01-15 18:32:20 +0900

    first

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import Text from '@shared/Text'
+
+function App() {
+  return (
+    <div>
+      <Text typography="t1" display="block" color="red">
+        t1
+      </Text>
+      <Text typography="t2">t2</Text>
+      <Text typography="t3">t3</Text>
+      <Text typography="t4">t3</Text>
+    </div>
+  )
+}
+
+export default App
