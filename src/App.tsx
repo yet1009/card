1090b7684056a14b6c40ed928789ccdecabfa8c0
yet1009 <yet1009@hanmail.net>
2024-01-17 08:28:16 +0900

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import Text from '@shared/Text'
+import Button from '@shared/Button'
 
 function App() {
   return (
@@ -11,6 +12,16 @@ function App() {
       <Text typography="t2">t2</Text>
       <Text typography="t3">t3</Text>
       <Text typography="t4">t3</Text>
+
+      <div style={{ height: 10, width: '100%', background: '#efefef' }}>
+        <Button color="success">클릭해주세요</Button>
+        <Button color="error">클릭해주세요</Button>
+        <Button color="error">클릭해주세요</Button>
+        <Button full={true} weak={true}>
+          클릭해주세요
+        </Button>
+        <Button>클릭해주세요</Button>
+      </div>
     </div>
   )
 }
